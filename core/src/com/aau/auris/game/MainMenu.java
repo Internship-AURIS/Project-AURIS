115fa7072f70e650944d38804667097c9aeda889
AndreasBitzan <andreas.bitzan@gmail.com>
2014-07-15 15:41:04 +0200

    Verbesserte Grafiken, Hintergrund Animation noch nicht fertig

--- core/src/com/aau/auris/game/MainMenu.java
+++ core/src/com/aau/auris/game/MainMenu.java
@@ -1,6 +1,5 @@
 package com.aau.auris.game;
 
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -11,6 +10,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -20,16 +20,18 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MainMenu implements Screen {
+	
+	private Texture region = new Texture(Gdx.files.internal("zeile1.png"));
+	private Animation parachuteAnimation;
+
+	public float runTime;
 
 	// BUTTON PROP.
 	private static final float BUTTON_WIDTH = 200f;
@@ -37,7 +39,7 @@ public class MainMenu implements Screen {
 	private static final float BUTTON_SPACING = 10f;
 	private static final float BUTTON_POS_X = 240f;
 	private static final float BUTTON_POS_Y = 300f;
-	//DESIGN:
+	// DESIGN:
 	Stage stage;
 	SpriteBatch batch;
 	Skin skin;
@@ -45,8 +47,8 @@ public class MainMenu implements Screen {
 	TextureAtlas atlas;
 	Table scoreTable;
 	private Game menugame;
-	
-	//SOUNNDS:
+
+	// SOUNNDS:
 	private TextButton btnStart;
 	private Sound hoverSound = Gdx.audio.newSound(Gdx.files
 			.internal("hover.wav"));
@@ -58,13 +60,29 @@ public class MainMenu implements Screen {
 			.internal("Theme2.wav"));
 	private Sound clickSound = Gdx.audio.newSound(Gdx.files
 			.internal("click.wav"));
-	//MUSIC:
+
+	// MUSIC:
 	public MainMenu(Game menugame) {
 		this.menugame = menugame;
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
 		create();
 
+		initAnimation();
+
+	}
+
+	private void initAnimation() {
+		runTime = 0;
+		final int COLS = 7, ROWS = 4;
+		TextureRegion[][] tmp = TextureRegion.split(region, region.getWidth()
+				/ COLS, region.getHeight() / ROWS);
+		TextureRegion[] swing = new TextureRegion[COLS];
+		for (int i = 0; i < COLS; i++) {
+			swing[i] = tmp[0][i];
+		}
+		parachuteAnimation = new Animation(0.1f, swing);
+		parachuteAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 	}
 
 	public void create() {
@@ -72,10 +90,10 @@ public class MainMenu implements Screen {
 		menuMusic.play();
 		batch = new SpriteBatch();
 
-		atlas = new TextureAtlas("buttons.pack");
+		atlas = new TextureAtlas("newbuttons.atlas");
 		skin = new Skin(atlas);
 		scoreTable = new Table();
-//		scoreTable.setFillParent(true);
+		// scoreTable.setFillParent(true);
 
 		Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
 		pixmap.setColor(Color.MAROON);
@@ -89,33 +107,33 @@ public class MainMenu implements Screen {
 		// DESGIN FÜR EXIT:
 
 		TextButtonStyle textbuttonStyleExit = new TextButtonStyle();
-		textbuttonStyleExit.up = skin.getDrawable("btnexit");
-		textbuttonStyleExit.down = skin.getDrawable("btnexit");
+		textbuttonStyleExit.up = skin.getDrawable("btnExit");
+		textbuttonStyleExit.down = skin.getDrawable("btnExit");
 		textbuttonStyleExit.checked = skin.newDrawable("white",
 				Color.LIGHT_GRAY);
-		textbuttonStyleExit.over = skin.getDrawable("btnexitover");
+		textbuttonStyleExit.over = skin.getDrawable("btnExitOver");
 		textbuttonStyleExit.font = skin.getFont("default");
 		skin.add("default", textbuttonStyleExit);
 
 		// DESIGN FÜR START:
-		
+
 		TextButtonStyle textbuttonStyleStart = new TextButtonStyle();
-		textbuttonStyleStart.up = skin.getDrawable("btnstart");
+		textbuttonStyleStart.up = skin.getDrawable("btnStart");
 		textbuttonStyleStart.down = skin.newDrawable("white", Color.MAROON);
 		textbuttonStyleStart.checked = skin.newDrawable("white",
 				Color.LIGHT_GRAY);
-		textbuttonStyleStart.over = skin.getDrawable("btnstartover");
+		textbuttonStyleStart.over = skin.getDrawable("btnStartOver");
 		textbuttonStyleStart.font = skin.getFont("default");
 		skin.add("start", textbuttonStyleStart);
-		
-		//DESIGN FÜR CREDTIS:
-		
+
+		// DESIGN FÜR CREDTIS:
+
 		TextButtonStyle textbuttonStyleCredits = new TextButtonStyle();
-		textbuttonStyleCredits.up = skin.getDrawable("btncredits");
+		textbuttonStyleCredits.up = skin.getDrawable("btnCredits");
 		textbuttonStyleCredits.down = skin.newDrawable("white", Color.MAROON);
 		textbuttonStyleCredits.checked = skin.newDrawable("white",
 				Color.LIGHT_GRAY);
-		textbuttonStyleCredits.over = skin.getDrawable("btncreditsover");
+		textbuttonStyleCredits.over = skin.getDrawable("btnCreditsOver");
 		textbuttonStyleCredits.font = skin.getFont("default");
 		skin.add("start", textbuttonStyleCredits);
 
@@ -132,17 +150,17 @@ public class MainMenu implements Screen {
 		btnExit.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 
 		Texture backgroundTexture = new Texture(
-				Gdx.files.internal("Backpic.png"));
+				Gdx.files.internal("testback.png"));
 		TextureRegion backTextRegion = new TextureRegion(backgroundTexture,
-				800, 600);
+				848, 480);
 		Image img = new Image(backTextRegion);
 		stage.addActor(img);
 		stage.addActor(btnCredits);
 		stage.addActor(btnStart);
 		stage.addActor(btnExit);
 
-		//LISTENER:
-		
+		// LISTENER:
+
 		btnStart.addListener(new ClickListener() {
 			public void touchUp(InputEvent event, float x, float y, int point,
 					int button) {
@@ -188,7 +206,6 @@ public class MainMenu implements Screen {
 			}
 
 		});
-		
 
 	}
 
@@ -196,12 +213,18 @@ public class MainMenu implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		scoreTable.debug(); // turn on all debug lines (table, cell, and widget)
 		scoreTable.debugTable();
-		
+
 		stage.act(delta);
 		stage.draw();
+
+		runTime += delta;
+		batch.begin();
+		batch.draw(parachuteAnimation.getKeyFrame(runTime, true), 100, 100);
+		System.out.println(runTime);
+		batch.end();
 
 	}
 
