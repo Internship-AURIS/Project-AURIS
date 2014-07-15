be9c8cbc37e26bef3391442e2da460d876d96464
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2014-07-15 12:55:32 +0200

    added loginscreen, highscore and save userdata(playername, score)

--- core/src/com/aau/auris/game/AURIS_Game.java
+++ core/src/com/aau/auris/game/AURIS_Game.java
@@ -1,18 +1,27 @@
 package com.aau.auris.game;
 
+import com.aau.auris.game.userdata.Player;
+import com.aau.auris.game.userdata.UserData;
 import com.badlogic.gdx.Game;
 
 public class AURIS_Game extends Game
 {
-
-	private GamePreferences preferences;
+	private UserData userdata = null;
+	private Player player = null;
 
 	@Override
 	public void create()
 	{
-		this.preferences = new GamePreferences();
+		userdata = new UserData();
+		userdata.load();
 
 		this.setScreen(new LoginScreen(this));
+	}
+
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+		System.out.println("player logged in: " + player);
 	}
 
 	@Override
@@ -21,8 +30,16 @@ public class AURIS_Game extends Game
 		super.render();
 	}
 
-	public GamePreferences getGamePreferences()
+	public UserData getUserData()
 	{
-		return preferences;
+		return userdata;
 	}
+
+	@Override
+	public void dispose()
+	{
+		super.dispose();
+		userdata.save();
+	}
+
 }
--- desktop/src/com/aau/auris/game/desktop/DesktopLauncher.java
+++ desktop/src/com/aau/auris/game/desktop/DesktopLauncher.java
@@ -1,14 +1,21 @@
 package com.aau.auris.game.desktop;
 
+import java.awt.Dimension;
+
 import com.aau.auris.game.AURIS_Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher
 {
+
+	public static final Dimension PROJECTOR_DIMENSION = new Dimension(848, 480);
+
 	public static void main(String[] arg)
 	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = PROJECTOR_DIMENSION.width;
+		config.height = PROJECTOR_DIMENSION.height;
 		new LwjglApplication(new AURIS_Game(), config);
 	}
 }
